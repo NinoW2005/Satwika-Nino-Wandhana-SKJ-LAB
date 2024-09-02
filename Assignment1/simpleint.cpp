467e5fb98fb0443d28012aae99d0b821b1a4d793
Satwika Nino Wandhana <150007590+NinoW2005@users.noreply.github.com>
2024-09-02 06:03:52 -0400

    Add files via upload

---
+++ Assignment1/simpleint.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int add(int x, int y){
+	int sum = x + y;
+	return sum;
+}
+
+int main() {
+	int a = 3;
+	int b = 2;
+
+	cout << add(a, b);
+
+return 0;
+
+}
